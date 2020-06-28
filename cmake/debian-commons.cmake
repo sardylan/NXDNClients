ee44fccf59f16f5c94c83cdc685a9a41333e6018
sardylan <sardylan@gmail.com>
2020-06-28 14:42:07 +0200

    Fixes cron filename

--- cmake/debian-commons.cmake
+++ cmake/debian-commons.cmake
@@ -12,7 +12,8 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/packaging/debian/nxdn.cron
         @ONLY)
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/packaging/debian/nxdn.cron
-        DESTINATION ${DESTDIR}/etc/cron.d/nxdn
+        DESTINATION ${DESTDIR}/etc/cron.d/
+        RENAME nxdn
         PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
         COMPONENT NXDNCommons)
 
